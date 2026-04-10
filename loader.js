c05a6265dd24657eb6ebc48d97985cf841c0989c
widgets0 <vvvamr@gmail.com>
2026-04-10 09:36:15 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,7 +1,12 @@
 (function() {
+    // Скрипт сам забирает "id" из ссылки (в данном случае "vtb-tours")
+    const urlParams = new URLSearchParams(document.currentScript.src.split('?')[1]);
+    const widgetId = urlParams.get('id'); 
+
     const frame = document.createElement('iframe');
-    // Сюда ты вставляешь ссылку на страницу конкретного клиента
-    frame.src = 'https://widgets0.github.io/widgets0/'; 
+    // Он сам подставляет папку клиента в путь!
+    frame.src = 'https://widgets0.github.io/widgets0/' + widgetId + '/index.html'; 
+    
     frame.style = "position: fixed; bottom: 20px; right: 20px; z-index: 999999; width: 380px; height: 620px; border: none; overflow: hidden;";
     frame.setAttribute('scrolling', 'no');
     document.body.appendChild(frame);
